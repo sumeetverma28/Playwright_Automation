04e7d537397f3648a5bf94fe60266f946bf3b588
sumeetverma <sumeet.verma@acldigital.com>
2026-08-19 16:34:40 +0530

    New changes

--- Leave_Page.py
+++ Leave_Page.py
@@ -1,15 +1,13 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-
     browser = p.chromium.launch(headless=False)
-
     page = browser.new_page()
-  
+
     def test_login():
         # Login with valid credentials
         page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        page.wait_for_load_state()   
+        page.wait_for_load_state()
         page.wait_for_timeout(5000)
         print(page.title())
         page.locator("//input[@name='username']").fill("Admin")
@@ -21,8 +19,13 @@ with sync_playwright() as p:
         page.wait_for_timeout(8000)
         page.locator("//span[text()='Leave']").click()
         page.wait_for_load_state()
-        page.locator("//span[@class='oxd-userdropdown-tab']").click()
-        page.locator("//ul[@class='oxd-dropdown-menu']").locator("//a[@href='/web/index.php/auth/logout']").click()
-    
+        page.wait_for_timeout(3000)
+        page.locator("(//input[@placeholder='yyyy-dd-mm'])[1]").clear()
+        page.wait_for_timeout(3000)
+        page.locator("(//input[@placeholder='yyyy-dd-mm'])[1]").fill("2024-06-01")
+        page.locator("(//input[@placeholder='yyyy-dd-mm'])[2]").fill("2024-06-30")
+        page.wait_for_timeout(3000)
+        browser.close()
+
+
     test_login()
-    browser.close()
